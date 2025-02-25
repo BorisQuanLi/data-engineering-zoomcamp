ae882ddb23c1464b1763ce3dd17e55b8400c9404
Boris Li <boris.quan.li@gmail.com>
2025-02-24 22:01:04 -0500

    get main.tf for a GCP Compute instance right

--- cohorts/2024/01-docker-terraform/follow-along/terraform-gcp-v2/main.tf
+++ cohorts/2024/01-docker-terraform/follow-along/terraform-gcp-v2/main.tf
@@ -10,25 +10,32 @@ terraform {
 }
 
 provider "google" {
-  project     = "zoomcamp-data-engineering"
-  region      = "us-east1"
-  zone        = "us-east1-b"
-  credentials = "gcp_keys/verdant-cargo-406919-2fdfe25d9f90.json"
+  project     = "verdant-cargo-406919"
+  region      = "us-east1"    # Changed to us-east1
+  zone        = "us-east1-b"  # Changed to us-east1-b (which we know exists from earlier)
+  # credentials = "gcp_keys/verdant-cargo-406919-2fdfe25d9f90.json"
+  # Make sure force_destroy is set to true for any storage resources
+  # to allow terraform destroy to work without manual intervention
 }
 
-resource "google_compute_instance" "ga-instance" {
+resource "google_compute_instance" "data_engineering_vm" {  # Changed from "ga-instance" to "data_engineering_vm"
   name         = "terraform-instance"
   machine_type = "f1-micro"
-  zone         = "us-east1-b"
+  zone         = "us-east1-b"  # Changed to us-east1-b
+
+  deletion_protection = false
 
   service_account {
     email  = "terraform-provisioner-zoomcamp@verdant-cargo-406919.iam.gserviceaccount.com"
-    scopes = ["https://www.googleapis.com/auth/cloud-platform"]
+    scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+      "https://www.googleapis.com/auth/compute"
+    ]
   }
 
   boot_disk {
     initialize_params {
-      image = "debian-cloud/debian-10"
+      image = "debian-cloud/debian-11"    # Changed from debian-10 to debian-11
     }
   }
 
