6e3bcd221bafce89725b0914c8e5e55064331e35
Boris Li <boris.quan.li@gmail.com>
2025-02-24 23:10:38 -0500

    get terraform-basic/main.tf right

--- cohorts/2025/01-docker-terraform/code_solution/terraform-gcp-solution/course_terraform_repo/terraform_basic/main.tf
+++ cohorts/2025/01-docker-terraform/code_solution/terraform-gcp-solution/course_terraform_repo/terraform_basic/main.tf
@@ -10,8 +10,8 @@ terraform {
 provider "google" {
   # Credentials only needs to be set if you do not have the GOOGLE_APPLICATION_CREDENTIALS set
   #  credentials = 
-  project = "zoomcamp-data-engineering"
-  region  = "us-central1"
+  project = "verdant-cargo-406919"  # Change this to match your service account's project
+  region  = "us-east1"
 }
 
 
@@ -19,6 +19,7 @@ provider "google" {
 resource "google_storage_bucket" "data-lake-bucket" {
   name     = "data_engineering_zoomcamp_module_01_exercise"
   location = "US"
+  project  = "verdant-cargo-406919"  # Add explicit project ID
 
   # Optional, but recommended settings:
   storage_class               = "STANDARD"
@@ -38,11 +39,37 @@ resource "google_storage_bucket" "data-lake-bucket" {
   }
 
   force_destroy = true
+
+  labels = {
+    environment = "development"
+    project     = "data-engineering-zoomcamp"
+    creator     = "terraform"
+  }
+
+  # Ensure consistent lifecycle rules
+  lifecycle_rule {
+    condition {
+      age = 30
+    }
+    action {
+      type = "Delete"
+    }
+  }
 }
 
 
 resource "google_bigquery_dataset" "dataset" {
   dataset_id = "imaginary_dataset_de_zoomcamp_module_01"
-  project    = "zoomcamp-data-engineering"
+  project    = "verdant-cargo-406919"  # Add explicit project ID
   location   = "US"
+
+  description = "Dataset for Data Engineering Zoomcamp Module 1"
+  
+  default_table_expiration_ms = 7776000000  # 90 days
+
+  labels = {
+    environment = "development"
+    project     = "data-engineering-zoomcamp"
+    creator     = "terraform"
+  }
 }
